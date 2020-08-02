8fd582d81edadf5186b3932202ad0afe67060a24
Akash Dandriyal <akash.d1409@gmail.com>
2020-08-02 18:06:40 +0530

    Bug fixed

--- appp.js
+++ appp.js
@@ -178,6 +178,6 @@ appp.get("/world", function(req, res){
 })
 
 var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number, function(){
+appp.listen(port_number, function(){
     console.log("Server is running on port "+ port_number);
 });
